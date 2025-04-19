69c289c91b67f87315766641d4fdfbd20fd60d70
Luis <luisotavio20novembro@gmail.com>
2025-04-19 20:20:56 -0300

    Creating Players and Enemies Moving Players and Enemies

--- code/const.py
+++ code/const.py
@@ -1,4 +1,4 @@
-
+import pygame
 
 #C
 COLOR_ORANGE = (255,128,0)
@@ -11,6 +11,10 @@ COLOR_YELLOW = (255,255,0)
 #E
 
 
+EVENT_ENEMY = pygame.USEREVENT + 1
+
+
+
 ENTITY_SPEED = {
 
     'Level1Bg0' : 0,
@@ -20,8 +24,17 @@ ENTITY_SPEED = {
     'Level1Bg4' : 4,
     'Level1Bg5' : 5,
     'Level1Bg6' : 6,
+    'Player1' : 3,
+    'Player2' : 3,
+    'Enemy1' : 2,
+    'Enemy2' : 1,
 
 }
+
+
+
+
+
 
 
 
@@ -35,6 +48,34 @@ MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COMPETITIVE',
                'SCORE',
                'EXIT')
+
+
+
+
+
+
+
+#P
+
+
+PLAYER_KEY_UP = {'Player1' : pygame.K_UP,
+                 'Player2' : pygame.K_w}
+PLAYER_KEY_DOWN = {'Player1' : pygame.K_DOWN,
+                   'Player2' : pygame.K_s}
+PLAYER_KEY_LEFT = {'Player1' : pygame.K_LEFT,
+                   'Player2' : pygame.K_a}
+PLAYER_KEY_RIGHT = {'Player1' : pygame.K_RIGHT,
+                    'Player2' : pygame.K_d}
+PLAYER_KEY_SHOOT = {'Player1' : pygame.K_RCTRL,
+                    'Player2' : pygame.K_LCTRL}
+
+
+
+
+
+#S
+
+SPAWN_TIME = 5000
 
 
 
